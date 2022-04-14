3355f3b679b7dbea033d10599b6cddd6623c6edb
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-04-13 21:14:04 -0600

    Add generate htmlmethod

--- Idea.js
+++ Idea.js
@@ -7,8 +7,29 @@ class Idea {
   }
 
   updateIdea() {
-    if (this.star) {
+    if (!this.star) {
     this.star = true;
+  } else if (this.star) {
+      this.star = false;
     }
+  }
+  generateHtml() {
+    let string = `<section class="idea-card-grid">
+      <article class="idea-card card">
+        <header class="idea-card header">
+          <img src="assets/star-active.svg">
+          <img src="assets/delete.svg">
+        </header>
+        <div class="text-holder">
+        <h3 class="title idea-card" id="${this.id}">${this.title}</h3>
+        <p id="new-body">${this.body}
+        </p>
+        </div>
+        <footer class="footer idea-card">
+          <img src="assets/comment.svg">
+          <p class="footer-text">Comment</p>
+        </footer>
+      </article>`
+      return string
   }
 }
